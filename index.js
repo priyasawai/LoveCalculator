52b99400919f42aee5949cb0c0a4c966c7379a7d
priyasawai <101619903+priyasawai@users.noreply.github.com>
2023-04-26 18:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 function checkloveper(){
-    var lovedata=Math.floor(Math.random()*100);
+    const name=document.getElementById("name").value;
+    const lname=document.getElementById("lname").value;
+    if(name=="")
+    {
+        alert("Please Enter Your Name");
+    }
+    else if(name.length<=2)
+    {
+        alert("Minimum length is 3");
+    }
+     else if(!isNaN(name)){
+        alert("Number is not allowed");
+    }
+
+    if(lname=="")
+    {
+        alert("Please Enter Your LoveName");
+    }
+    else if(lname.length<=2)
+    {
+        alert("Minimum length is 3");
+    }
+     else if(!isNaN(lname)){
+        alert("Number is not allowed");
+    }
+
+    else{
+        var lovedata=Math.floor(Math.random()*100);
    // console.log(lovedata);
    document.getElementById("lovevalue").value=lovedata+"%";
+}   
 }
